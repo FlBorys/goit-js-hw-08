7ff8db59a2747063b82bf9500c16e377e2e3dcd9
FlBorys <131404105+FlBorys@users.noreply.github.com>
2023-09-15 02:50:41 +0300

    4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+
+    const formEl = document.querySelector('.feedback-form');
+    const emailInput = formEl.querySelector('input[name="email"]');
+    const messageInput = formEl.querySelector('[name="message"]');
+    
+    formEl.addEventListener('input', throttle(saveFormData, 500));
+    formEl.addEventListener('submit', onSubmit);
+    
+    function saveFormData() {
+        const dataForm = {
+            email: emailInput.value,
+            message: messageInput.value
+        }
+        localStorage.setItem("feedback-form-state", JSON.stringify(dataForm));
+    }
+    
+    function onSubmit(e) {
+        e.preventDefault();
+        emailInput.value = '';
+        messageInput.value = '';
+
+        localStorage.removeItem("feedback-form-state");
+
+    }
+    
+    function dataStorage() {
+        const data = localStorage.getItem("feedback-form-state");
+        if (data !== null) {
+            const dataForm = JSON.parse(data);
+            console.log(dataForm);
+            emailInput.value = dataForm.email;
+            messageInput.value = dataForm.message;
+
+        }
+    }
 
